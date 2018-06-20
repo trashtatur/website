0c335ba1b0a4868ce441db876e81f67a84deb27e
Andreas Hörster <andreas-hoerster@web.de>
2018-06-20 04:24:49 +0200

    Made npm install on root level install all modules, even module ones

---
+++ preinstall.js
@@ -0,0 +1,34 @@
+let fs = require('fs');
+let resolve = require('path').resolve;
+let join = require('path').join;
+let cp = require('child_process');
+
+// get module paths
+let cM = resolve(__dirname, 'core/core_modules');
+let venM = resolve(__dirname, 'ven/ven_modules');
+
+
+//for core modules
+fs.readdirSync(cM)
+    .forEach(function (mod) {
+        let modPath = join(cM, mod);
+
+        // ensure path has package.json
+        if (!fs.existsSync(join(modPath, 'package.json'))) return;
+
+        // install folder
+        cp.spawn('npm', ['install','--save'], { env: process.env, cwd: modPath, stdio: 'inherit' })
+    });
+
+
+//For outside modules
+fs.readdirSync(venM)
+    .forEach(function (mod) {
+        let modPath = join(venM, mod);
+
+        // ensure path has package.json
+        if (!fs.existsSync(join(modPath, 'package.json'))) return;
+
+        // install folder
+        cp.spawn('npm', ['install','--save'], { env: process.env, cwd: modPath, stdio: 'inherit' })
+    });
