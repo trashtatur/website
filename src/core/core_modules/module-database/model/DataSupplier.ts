3cc6fbf94c3b41c17bd9273d80d1c3eb145e1f6a
12345Felix <Felix.Kirsch@gmx.net>
2018-09-06 21:35:47 +0200

    comment

--- src/core/core_modules/module-database/model/DataSupplier.ts
+++ src/core/core_modules/module-database/model/DataSupplier.ts
@@ -1,12 +1,30 @@
 var config = require('../../../../../modelConfig.json');
-
+/**
+ * @author 
+ * 
+ * @description
+ * @version Erstellungsdatum
+ * @version Datum der neusten Änderung
+ *          Beschreibung der Änderung
+ * 
+*/
 export class DataSupplier {
 
+    //////////////////////////////// instance variables
     private readonly databaseHost:string;
     private readonly databaseName:string;
     private readonly databasePassword:string;
     private readonly databaseUser:string;
 
+    /**
+ * @author 
+ * 
+ * @description
+ * @version Erstellungsdatum
+ * @version Datum der neusten Änderung
+ *          Beschreibung der Änderung
+ * 
+*/
     constructor() {
         this.databaseHost = process.env.WEBSITE_DB_HOST ? process.env.WEBSITE_DB_HOST : config.host;
         this.databaseName = process.env.WEBSITE_DB_NAME ? process.env.WEBSITE_DB_NAME : config.dbName;
@@ -14,6 +32,8 @@ export class DataSupplier {
         this.databaseUser = process.env.WEBSITE_DB_USER ? process.env.WEBSITE_DB_USER : config.dbUser;
     }
 
+    ///////////////////////////////////////// getter and setter
+    
     getDatabaseHost() {
         return this.databaseHost;
     }
