28bb97bd842d7965d37ae34d6cae37d9f6bbc713
12345Felix <Felix.Kirsch@gmx.net>
2018-09-06 21:37:19 +0200

    comment

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@ let database =  require('./core/core_modules/module-database');
 let controllerRegistrar = require('./ControllerRegistrar');
 let staticRegistrar = require('./StaticsRegistrar');
 let bundler = require('./bundler');
+/**
+ * @author 
+ * 
+ * @description
+ * @version Erstellungsdatum
+ * @version Datum der neusten Änderung
+ *          Beschreibung der Änderung
+ * 
+*/
 controllerRegistrar.registerControllers();
 staticRegistrar.registerTemplates();
 bundler.bundleJSDependancies();
 webserver.startServer();
+
 
 
