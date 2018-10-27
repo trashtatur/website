b02f8d4927ee2fd912d1177be137e273022c4d57
Andreas Hörster <andreas-hoerster@web.de>
2018-10-27 19:05:10 +0200

    [GULP] Added logger to gulp processes

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,30 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-var browserify = require('browserify');
-var path = require('path');
-var tap = require('gulp-tap');
-var buffer = require('gulp-buffer');
-var sourcemaps = require('gulp-sourcemaps');
+const browserify = require('browserify');
+const path = require('path');
+const logger = require('gulp-logger');
+const tap = require('gulp-tap');
+const buffer = require('gulp-buffer');
+const sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('default',function () {
-    gulp.run('sass');
-    gulp.run('browserify')
+gulp.task('default',['sass','browserify'],function () {
+    logger({
+        after: 'ALL DONE'
+    })
 });
 
 gulp.task('sass', function () {
     gulp.src(__dirname+'/src/**/frontend/css/*.scss')
         .pipe(sass().on('error', sass.logError))
+
         .pipe(gulp.dest(function (file) {
             return file.base.replace('/src','/build')
+        }))
+        .pipe(logger({
+            before: 'Starting SASS Compilation',
+            after: 'All SASS files compiled',
         }));
 });
 
@@ -39,6 +46,9 @@ gulp.task('browserify', function () {
         .pipe(sourcemaps.init({loadMaps: true}))
         // write sourcemaps
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('build'));
-
+        .pipe(gulp.dest('build'))
+        .pipe(logger({
+            before: 'Starting bundling process',
+            after: 'All files bundled',
+        }));
 });
